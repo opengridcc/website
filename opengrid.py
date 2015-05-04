16059d5b7f327928c2a3e44917c5db39b8b13546
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-05-04 09:55:16 +0200

    fixed issue with tokens
    Fixed vulnerability where all sensor info was published on the website.
    + set debug to False

--- opengrid.py
+++ opengrid.py
@@ -22,7 +22,10 @@ def index():
 
 @app.route("/flukso/<fluksoid>")
 def flukso(fluksoid):
-    return render_template('flukso.html', flukso=hp.fluksosensors[fluksoid])
+    return render_template('flukso.html', 
+    	#flukso=hp.fluksosensors[fluksoid]
+    	flukso=fluksoid)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=False,host='0.0.0.0',port=5000)
+    #app.run(debug=True)
