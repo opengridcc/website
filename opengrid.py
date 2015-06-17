f737ad3ef7954a38424493ae9d7eb782dee44427
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-06-17 14:50:24 +0200

    Embedded figures

--- opengrid.py
+++ opengrid.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys, os
 from collections import namedtuple
-from flask import Flask, render_template, send_file, flash, redirect, url_for
+from flask import Flask, render_template, send_file, flash, redirect, url_for, safe_join, request
 
 import config
 
@@ -39,39 +39,65 @@ def sensor(sensorid):
       analyses=analyses
       )
 
-@app.route("/standby_horizontal/<sensorid>.png")
+@app.route("/standby_horizontal/<sensorid>")
 def standby_horizontal(sensorid):
-    path = c.get('backend','figures')
-    filename = path + '/standby_horizontal_'+sensorid+'.png'
 
-    if not os.path.exists(filename):
+    filename = 'standby_horizontal_'+sensorid+'.png'
+
+    if not figure_exists(filename):
         flash('No standby_horizontal graph found for this sensor')
         return redirect(url_for('sensor', sensorid=sensorid))
 
-    return send_file(filename, mimetype='image/png')
+    return render_template('analysis_image.html',
+        analysisname = 'Standby Horizontal',
+        filename = filename,
+        sensorid = sensorid)
 
-@app.route("/standby_vertical/<sensorid>.png")
+@app.route("/standby_vertical/<sensorid>")
 def standby_vertical(sensorid):
 
-    path = c.get('backend','figures')
-    filename = path + '/standby_vertical_'+sensorid+'.png'
+    filename = 'standby_vertical_'+sensorid+'.png'
 
-    if not os.path.exists(filename):
+    if not figure_exists(filename):
         flash('No standby_vertical graph found for this sensor')
         return redirect(url_for('sensor', sensorid=sensorid))
 
-    return send_file(filename, mimetype='image/png')
+    return render_template('analysis_image.html',
+        analysisname = 'Standby Vertical',
+        filename = filename,
+        sensorid = sensorid)
 
-@app.route("/timeseries/<sensorid>.html")
+@app.route("/timeseries/<sensorid>")
 def timeseries(sensorid):
-    path = c.get('backend','figures')
-    filename = path + '/TimeSeries_'+sensorid+'.html'
 
-    if not os.path.exists(filename):
+    path = c.get('backend','figures')
+    filename = 'TimeSeries_'+sensorid+'.html'
+    file = safe_join(path,filename)
+
+    if not os.path.exists(file):
         flash('No timeseries graph found for this sensor')
         return redirect(url_for('sensor', sensorid=sensorid))
 
-    return send_file(filename, mimetype='text/html')
+    with open (file, "r") as myfile:
+        content = myfile.read()
+
+    return render_template('analysis_html.html',
+        analysisname = 'Time Series',
+        sensorid = sensorid,
+        content = content)
+
+@app.route("/figures/<filename>")
+def figure(filename):
+    path = c.get('backend','figures')
+    file = safe_join(path,filename)
+
+    return send_file(file)
+
+def figure_exists(filename):
+    path = c.get('backend','figures')
+    file = safe_join(path,filename)
+
+    return os.path.exists(file)
 
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0',port=5000) #TODO: implement switch between development and production mode in config file
