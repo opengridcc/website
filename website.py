c7ce88f11f2b7f78e3b029f8691efdc079875a2a
Jan Pecinovsky <janpecinovsky@gmail.com>
2016-01-13 12:12:45 +0100

    Minor improvements for legibility

--- website.py
+++ website.py
@@ -24,34 +24,36 @@ def index():
 
 @app.route("/flukso/<fluksoid>")
 def flukso(fluksoid):
-    flukso = hp.find_device(fluksoid)
+    f = hp.find_device(fluksoid)
 
     return render_template(
             'flukso.html',
-            flukso=flukso,
-            sensors = flukso.get_sensors()
+            flukso=f,
+            sensors=f.get_sensors()
     )
 
 
 @app.route("/sensor/<sensorid>")
 def sensor(sensorid):
 
-    sensor = hp.find_sensor(sensorid)
+    s = hp.find_sensor(sensorid)
 
     analyses = ['timeseries']
-    if sensor.type == 'electricity' and not sensor.system == 'solar':
+    if s.type == 'electricity' and not s.system == 'solar':
         analyses.append('standby_horizontal')
         analyses.append('standby_vertical')
     
-    return render_template('sensor.html',
-      sensor=sensor,
-      analyses=analyses
-      )
+    return render_template(
+            'sensor.html',
+            sensor=s,
+            analyses=analyses
+    )
+
 
 @app.route("/standby_horizontal/<sensorid>")
 def standby_horizontal(sensorid):
 
-    sensor = hp.find_sensor(sensorid)
+    s = hp.find_sensor(sensorid)
 
     filename = 'standby_horizontal_'+sensorid+'.png'
 
@@ -59,61 +61,70 @@ def standby_horizontal(sensorid):
         flash('No standby_horizontal graph found for this sensor')
         return redirect(url_for('sensor', sensorid=sensorid))
 
-    return render_template('analysis_image.html',
-        analysisname = 'Standby Horizontal',
-        filename = filename,
-        sensor = sensor)
+    return render_template(
+            'analysis_image.html',
+            analysisname='Standby Horizontal',
+            filename=filename,
+            sensor=s
+    )
+
 
 @app.route("/standby_vertical/<sensorid>")
 def standby_vertical(sensorid):
 
-    sensor = hp.find_sensor(sensorid)
+    s = hp.find_sensor(sensorid)
 
-    filename = 'standby_vertical_'+sensorid+'.png'
+    filename = 'standby_vertical_{}.png'.format(s.key)
 
     if not figure_exists(filename):
         flash('No standby_vertical graph found for this sensor')
         return redirect(url_for('sensor', sensorid=sensorid))
 
-    return render_template('analysis_image.html',
-        analysisname = 'Standby Vertical',
-        filename = filename,
-        sensor = sensor)
+    return render_template(
+            'analysis_image.html',
+            analysisname = 'Standby Vertical',
+            filename = filename,
+            sensor = s)
+
 
 @app.route("/timeseries/<sensorid>")
 def timeseries(sensorid):
 
-    sensor = hp.find_sensor(sensorid)
+    s = hp.find_sensor(sensorid)
 
     path = c.get('backend','figures')
-    filename = 'TimeSeries_'+sensorid+'.html'
-    file = safe_join(path,filename)
+    filename = 'TimeSeries_{}.html'.format(s.key)
+    file_path = safe_join(path,filename)
 
-    if not os.path.exists(file):
+    if not os.path.exists(file_path):
         flash('No timeseries graph found for this sensor')
         return redirect(url_for('sensor', sensorid=sensorid))
 
-    with open (file, "r") as myfile:
-        content = myfile.read()
+    with open (file_path, "r") as html_graph:
+        content = html_graph.read()
 
-    return render_template('analysis_html.html',
-        analysisname = 'Time Series',
-        sensor = sensor,
-        content = content)
+    return render_template(
+            'analysis_html.html',
+            analysisname='Time Series',
+            sensor=s,
+            content=content
+    )
+
 
 @app.route("/figures/<filename>")
 def figure(filename):
     path = c.get('backend','figures')
-    file = safe_join(path,filename)
+    file_path = safe_join(path,filename)
 
-    return send_file(file)
+    return send_file(file_path)
+
 
 def figure_exists(filename):
     path = c.get('backend','figures')
-    file = safe_join(path,filename)
+    file_path = safe_join(path,filename)
 
-    return os.path.exists(file)
+    return os.path.exists(file_path)
 
 if __name__ == "__main__":
-    #app.run(debug=False,host='0.0.0.0',port=5000) #TODO: implement switch between development and production mode in config file
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0',port=5000) #TODO: implement switch between development and production mode in config file
+    #app.run(debug=True)
